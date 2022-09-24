1c71551b4ad5961479ec7e63af4c33fc9de74aed
Tamzid Israk <tamzidisrak12@gmail.com>
2022-09-24 23:11:31 +0600

    wallpaper app with admin panel in node js

---
+++ public/js/app.js
@@ -0,0 +1,36 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBwWinf_fLMZ3g3ZTNZUA5W-VW5pqnrdFk",
+//     authDomain: "wallpaper-e3aa0.firebaseapp.com",
+//     projectId: "wallpaper-e3aa0",
+//     storageBucket: "wallpaper-e3aa0.appspot.com",
+//     messagingSenderId: "817059987854",
+//     appId: "1:817059987854:web:b3ebb016e17458879524df",
+//     measurementId: "G-TN2VJYD42Y"
+//   };
+//    firebase.initializeApp(firebaseConfig);
+//   // Initialize Firebase
+//  // const app = initializeApp(firebaseConfig);
+
+//   //  firebase.auth.Auth.Persistence.Local;
+
+// $("#LoginBtn").click(function(){
+//    var email = $("#email").val;
+//    var pass = $("#password").val;
+   
+
+//   signInWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed in 
+//     const user = userCredential.user;
+//     // ...
+//     alert("successfully login ");
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+
+//     alert(errorMessage);
+//   });
+
+   
+// });
